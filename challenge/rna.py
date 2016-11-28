3aa6db9cead8a49849dc8494840b9faa8cfe77a6
Alex Grasley <alex@grasley.com>
2016-11-27 22:30:20 -0800

    all done with challenge

--- challenge/rna.py
+++ challenge/rna.py
@@ -1,5 +1,8 @@
 from heapq import heapify, heappop, heappush
 from itertools import islice
+from random import randint
+from numbers import Number
+
 
 def memoize(f):
     d = dict()
@@ -15,6 +18,21 @@ def memoize(f):
     return wrapper
 
 pairs = frozenset(['AU', 'GC', 'GU', 'UA', 'CG', 'UG'])
+
+
+def _qselect(k, a):
+    if a == [] or k < 1 or k > len(a):
+        return None
+    else:
+        pindex = randint(0, len(a)-1)
+        a[0], a[pindex] = a[pindex], a[0]
+        pivot = a[0]
+        left = [x for x in a if x < pivot]
+        right = [x for x in a[1:] if x >= pivot]
+        lleft = len(left)
+        return pivot if k == lleft+1 else \
+            _qselect(k, left) if k <= lleft else \
+            _qselect(k-lleft-1, right)
 
 
 def best(s):
@@ -60,8 +78,7 @@ def total(s):
             for k in xrange(i+1, j):
                 if s[i] + s[k] in pairs:
                     tot += t(i+1, k) * t(k+1, j)
-            q = tot + t(i+1, j)
-            return q
+            return tot + t(i+1, j)
 
     return t(0, len(s))
 
@@ -86,59 +103,73 @@ def kbest(s, kth):
                 cntr = None
         gens[(i, j)] = (gen, cntr)
         if cntr is None:
-            return None
+            return None, None
         else:
             return iterDict[(i, j, n)]
 
     def kb(i, j):
         if j - i < 2:
-            yield 0
+            yield 0, None
         else:
             h, used = [], set()
 
             def trypush(k, ni, nj):
-                cand1 = iter8(i+1, k, ni)
-                cand2 = iter8(k+1, j, nj)
+                cand1, _ = iter8(i+1, k, ni)
+                cand2, _ = iter8(k+1, j, nj)
                 if cand1 is not None and cand2 is not None and (k, ni, nj) not in used:
                     heappush(h, (-(cand1 + cand2 + 1), k, ni, nj))
                     used.add((k, ni, nj))
 
             for k in xrange(i+1, j):
                 if s[i] + s[k] in pairs:
-                    h.append((-(iter8(i+1, k, 0) + iter8(k+1, j, 0) + 1), k, 0, 0))
+                    h.append((-(iter8(i+1, k, 0)[0] + iter8(k+1, j, 0)[0] + 1), k, 0, 0))
                     used.add((k, 0, 0))
-            h.append((-(iter8(i+1, j, 0)), None, 0, None))
+            h.append((-(iter8(i+1, j, 0)[0]), None, 0, None))
+            if len(h) > kth:
+                pivot = _qselect(kth, h)
+                new_h = []
+                for x in h:
+                    if x < pivot:
+                        new_h.append(x)
+                new_h.append(pivot)
+                h = new_h
             heapify(h)
             while len(h):
                 val, k, ni, nj = heappop(h)
-                yield -(val)
                 if k is not None:
-                    trypush(k, ni + 1, nj)
-                    trypush(k, ni, nj + 1)
+                    yield -(val), (k, ni, nj)
+                    trypush(k, ni+1, nj)
+                    trypush(k, ni, nj+1)
                 else:
-                    cand = iter8(i+1, j, ni+1)
+                    yield -(val), ni
+                    cand, _ = iter8(i+1, j, ni+1)
                     if cand is not None:
                         heappush(h, (-cand, None, ni+1, None))
 
-    return list(islice(kb(0, len(s)), kth))
+    def backtrace(i, j, x):
+        backtrace_results = ['.'] * len(s)
 
+        def bt(i, j, x):
+            if x is None:
+                return
+            elif isinstance(x, Number):
+                _, new_x = iter8(i+1, j, x)
+                bt(i+1, j, new_x)
+            else:
+                k, ni, nj = x
+                backtrace_results[i] = '('
+                backtrace_results[k] = ')'
+                _, new_x = iter8(i+1, k, ni)
+                bt(i+1, k, new_x)
+                _, new_x = iter8(k+1, j, nj)
+                bt(k+1, j, new_x)
 
-print best("UUCAGGA")  # 3
-print best("GUUAGAGUCU")  # 4
-print best("GCACG")  # 2
-print best("AUAACCUUAUAGGGCUCUG")  # 8
-print best("UUGGACUUGAGAAAAG")  # 5
-print best("UCAAUGGGUAGUAAAU")  # 6
-print best("UUUGGCACUUUCAGA")  # 6
-print best("ACACACCUUGUCCGUGAA")  # 6
-print best("GAUGCCGUGUAGUCCAAAGACUUCACCGUUGG")  # 14
-print best("CGCGAAUAAAAAGGCACUGUU")  # 8
-print best("ACGGCCAGUAAAGGUCAUAUACGCGGAAUGACAGGUCUAUCUAC")  # 19
-print best("UGGGUGAGUCGCACACUCUGCGUACUCUUUCCGUAAUU")  # 15
-print best("AUACGUCGGGGACAAGAAUUACGG")  # 8
-print best("AGGCAUCAAACCCUGCAUGGGAGCACCGCCACUGGCGAUUUUGGUA")  # 20
-print best("CGAGGUGGCACUGACCAAACACCACCGAAAC")  # 9
-print best("CGCCGUCCGGGCGCGCCUUUUACGUAGAUUU")  # 12
-print best("CAUCGGGGUCUGAGAUGGCCAUGAAGGGCACGUACUGUUU")  # 18
-print best("AACCGCUGUGUCAAGCCCAUCCUGCCUUGUU")  # 11
-print best("")  # 0
+        bt(i, j, x)
+        return ''.join(backtrace_results)
+
+    results = []
+
+    for v, x in islice(kb(0, len(s)), kth):
+        results.append((v, backtrace(0, len(s), x)))
+
+    return results
